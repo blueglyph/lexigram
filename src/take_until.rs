67745cfaf19270adf8638dba906f9b8203ec3468
Redglyph <redglyph@gmail.com>
2024-02-16 15:12:00 +0100

    simplify take_until

--- src/take_until.rs
+++ src/take_until.rs
@@ -7,14 +7,16 @@ pub struct TakeUntil<I, P> {
     predicate: P
 }
 
-impl<I, P> TakeUntil<I, P>
-    where I: Iterator,
-          P: FnMut(&I::Item) -> bool
-{
-    fn new(iter: I, predicate: P) -> Self {
-        TakeUntil { iter, end: false, predicate }
-    }
-}
+// If new() is required (not here since only one instantiation below):
+//
+// impl<I, P> TakeUntil<I, P>
+//     where I: Iterator,
+//           P: FnMut(&I::Item) -> bool
+// {
+//     fn new(iter: I, predicate: P) -> Self {
+//         TakeUntil { iter, end: false, predicate }
+//     }
+// }
 
 impl<I, P> Iterator for TakeUntil<I, P>
     where I: Iterator,
@@ -43,7 +45,8 @@ pub trait TakeUntilIterator<T, P>: Iterator<Item=T>
     where P: FnMut(&T) -> bool
 {
     fn take_until(self, predicate: P) -> TakeUntil<Self, P> where Self: Sized {
-        TakeUntil::new(self, predicate)
+        // TakeUntil::new(self, predicate)
+        TakeUntil { iter: self, end: false, predicate }
     }
 }
 
@@ -53,69 +56,38 @@ impl<T, I: Iterator<Item=T>, P: FnMut(&T) -> bool> TakeUntilIterator<T, P> for I
 
 #[cfg(test)]
 mod tests {
-    use std::iter::TakeWhile;
     use super:: *;
 
     #[test]
-    fn predicate_take_until_normal() {
+    fn adapter_take_until() {
         let v = vec![1, 2, 3, 4, 5];
-        let iter = v.iter();
-        let mut iter_until = TakeUntil::new(iter, |&x| *x >= 4 );
-        let mut result: Vec<i32> = Vec::new();
-        while let Some(value) = iter_until.next() {
-            result.push(*value);
-        }
+        let result = v.iter().take_until(|&x| *x >= 4).map(|&n| n).collect::<Vec<_>>();
         assert_eq!(result, vec![1, 2, 3, 4]);
     }
 
+    // For comparison:
+    //      let v = vec![1, 2, 3, 4, 5];
+    //      let result = v.iter().take_while(|&x| *x < 4).map(|&n| n).collect::<Vec<_>>();
+    //      assert_eq!(result, vec![1, 2, 3]);
+
     #[test]
     fn predicate_take_until_empty() {
-        let v = vec![];
-        let iter = v.iter();
-        let mut iter_until = TakeUntil::new(iter, |&x| *x >= 4 );
-        let mut result: Vec<i32> = Vec::new();
-        while let Some(value) = iter_until.next() {
-            result.push(*value);
-        }
-        assert_eq!(result, vec![]);
+        let v: Vec<i32> = vec![];
+        let result = v.iter().take_until(|&x| *x >= 4).map(|&n| n).collect::<Vec<_>>();
+        assert_eq!(result.len(), 0);
     }
 
     #[test]
     fn predicate_take_until_false() {
         let v = vec![1, 2, 3, 4, 5];
-        let iter = v.iter();
-        let mut iter_until = TakeUntil::new(iter, |&x| *x > 10 );
-        let mut result: Vec<i32> = Vec::new();
-        while let Some(value) = iter_until.next() {
-            result.push(*value);
-        }
+        let result = v.iter().take_until(|&x| *x > 10).map(|&n| n).collect::<Vec<_>>();
         assert_eq!(result, vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn predicate_take_until_true() {
         let v = vec![1, 2, 3, 4, 5];
-        let iter = v.iter();
-        let mut iter_until = TakeUntil::new(iter, |&x| *x < 10 );
-        let mut result: Vec<i32> = Vec::new();
-        while let Some(value) = iter_until.next() {
-            result.push(*value);
-        }
+        let result = v.iter().take_until(|&x| *x < 10).map(|&n| n).collect::<Vec<_>>();
         assert_eq!(result, vec![1]);
     }
-
-    #[test]
-    fn adapter_take_until() {
-        let v = vec![1, 2, 3, 4, 5];
-        let result = v.iter().take_until(|&x| *x >= 4).collect::<Vec<_>>();
-        assert_eq!(result, vec![&1, &2, &3, &4]);
-   }
-
-    #[test]
-    fn adapter_take_while() {
-        let v = vec![1, 2, 3, 4, 5];
-        let result = v.iter().take_while(|&x| *x < 4).collect::<Vec<_>>();
-        assert_eq!(result, vec![&1, &2, &3]);
-   }
-
 }
