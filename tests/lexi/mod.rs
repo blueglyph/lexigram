7a74aae755f3c8b42e1e01087d60a699944cbc77
Redglyph <redglyph@gmail.com>
2024-12-20 17:35:09 +0100

    create lexi stub test (WIP)
    panics, to debug

--- tests/lexi/mod.rs
+++ tests/lexi/mod.rs
@@ -1,65 +1,82 @@
 use crate::gen::lexiparser::lexiparser::*;
 use crate::gen::lexiparser::lexiparser_types::*;
 
-struct LexiListener {}
+struct LexiListener {
+    verbose: bool
+}
 
 impl LexiListener {
     fn new() -> Self {
-        LexiListener {}
+        LexiListener {
+            verbose: false
+        }
     }
 }
 
 impl LexiParserListener for LexiListener {
     fn exit_file(&mut self, _ctx: CtxFile) -> SynFile {
-        todo!()
+        if self.verbose { println!("exit_file"); }
+        SynFile()
     }
 
     fn exit_file_item(&mut self, _ctx: CtxFileItem) -> SynFileItem {
-        todo!()
+        if self.verbose { println!("exit_file_item"); }
+        SynFileItem()
     }
 
     fn exit_header(&mut self, _ctx: CtxHeader) -> SynHeader {
-        todo!()
+        if self.verbose { println!("exit_header"); }
+        SynHeader()
     }
 
     fn exit_declaration(&mut self, _ctx: CtxDeclaration) -> SynDeclaration {
-        todo!()
+        if self.verbose { println!("exit_declaration"); }
+        SynDeclaration()
     }
 
     fn exit_option(&mut self, _ctx: CtxOption) -> SynOption {
-        todo!()
+        if self.verbose { println!("exit_option"); }
+        SynOption()
     }
 
     fn exit_rule(&mut self, _ctx: CtxRule) -> SynRule {
-        todo!()
+        if self.verbose { println!("exit_rule"); }
+        SynRule()
     }
 
     fn exit_actions(&mut self, _ctx: CtxActions) -> SynActions {
-        todo!()
+        if self.verbose { println!("exit_actions"); }
+        SynActions()
     }
 
     fn exit_action(&mut self, _ctx: CtxAction) -> SynAction {
-        todo!()
+        if self.verbose { println!("exit_action"); }
+        SynAction()
     }
 
     fn exit_match1(&mut self, _ctx: CtxMatch) -> SynMatch {
-        todo!()
+        if self.verbose { println!("exit_match1"); }
+        SynMatch()
     }
 
     fn exit_alt_items(&mut self, _ctx: CtxAltItems) -> SynAltItems {
-        todo!()
+        if self.verbose { println!("exit_alt_items"); }
+        SynAltItems()
     }
 
     fn exit_alt_item(&mut self, _ctx: CtxAltItem) -> SynAltItem {
-        todo!()
+        if self.verbose { println!("exit_alt_item"); }
+        SynAltItem()
     }
 
     fn exit_repeat_item(&mut self, _ctx: CtxRepeatItem) -> SynRepeatItem {
-        todo!()
+        if self.verbose { println!("exit_repeat_item"); }
+        SynRepeatItem()
     }
 
     fn exit_item(&mut self, _ctx: CtxItem) -> SynItem {
-        todo!()
+        if self.verbose { println!("exit_item"); }
+        SynItem()
     }
 }
 
@@ -74,12 +91,27 @@ mod tests {
 
     #[test]
     fn lexer_parser() {
-        let input = LEXICON;
-        let stream = CharReader::new(Cursor::new(input));
-        let mut lexer = build_lexer();
-        lexer.attach_stream(stream);
+        let tests = vec![
+            "",
+            "lexicon LexiLexer;",
+            LEXICON,
+        ];
+        const VERBOSE: bool = true;
 
-        let parser = build_parser();
-        let listener = LexiListener::new();
+        for (test_id, input) in tests.into_iter().enumerate() {
+            if VERBOSE { println!("// {:=<80}\n// Test {test_id}", ""); }
+            let stream = CharReader::new(Cursor::new(input));
+            let mut lexer = build_lexer();
+            lexer.attach_stream(stream);
+
+            let mut parser = build_parser();
+            let mut listener = LexiListener::new();
+            listener.verbose = VERBOSE;
+            let mut wrapper = ListenerWrapper::new(listener, false);
+            wrapper.set_verbose(VERBOSE);
+
+            let tokens = lexer.tokens().filter_map(|(tok, ch, text)| if ch == 0 { Some((tok, text)) } else { None });
+            let result = parser.parse_stream(&mut wrapper, tokens);
+        }
     }
 }
