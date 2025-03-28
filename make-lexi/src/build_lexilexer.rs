addc1b6dfa78caccdaef0ddadd0a6c8b1991bdf6
Redglyph <redglyph@gmail.com>
2025-03-28 16:52:17 +0100

    create new make-lexi package to build lexi's sources

---
+++ make-lexi/src/build_lexilexer.rs
@@ -0,0 +1,87 @@
+// Copyright (c) 2025 Redglyph (@gmail.com). All Rights Reserved.
+
+use std::fs::File;
+use std::io::BufReader;
+use lexi::lexi::Lexi;
+use lexigram::CollectJoin;
+use lexigram::io::CharReader;
+use lexigram::lexergen::LexerGen;
+use lexigram::parser::ParserError;
+use lexigram::test_tools::replace_tagged_source;
+use crate::*;
+
+fn lexilexer_source(lexicon_filename: &str, indent: usize, verbose: bool) -> Result<(String, String), ParserError> {
+    let file = File::open(lexicon_filename).expect(&format!("couldn't open lexicon file {lexicon_filename}"));
+    let reader = BufReader::new(file);
+    let stream = CharReader::new(reader);
+    let mut lexi = Lexi::new();
+    let result = lexi.build(stream);
+    let mut listener = lexi.wrapper.listener();
+    if verbose {
+        let msg = listener.get_log().get_messages().map(|s| format!("- {s:?}")).join("\n");
+        if !msg.is_empty() {
+            println!("Parser messages:\n{msg}");
+        }
+        let msg = listener.get_log().get_messages().map(|s| format!("- {s:?}")).join("\n");
+        if !msg.is_empty() {
+            println!("Listener messages:\n{msg}");
+        }
+    }
+    if let Err(error) = result {
+        return Err(error);
+    }
+    let symbol_table = listener.build_symbol_table();
+    if verbose {
+        println!("Rules lexicon {}:\n{}", listener.get_name(), listener.rules_to_string(0));
+    }
+    // - builds the dfa from the reg tree
+    let dfa = listener.make_dfa().optimize();
+
+    // - builds the lexer
+    let mut lexgen = LexerGen::new();
+    lexgen.max_utf8_chars = 0;
+    lexgen.from_dfa(dfa);
+    lexgen.symbol_table = Some(symbol_table);
+    let sym_src = lexgen.build_symbols_source_code(0).expect("symbol source code");
+    if verbose {
+        // terminals to replace in src/lexi/mod.rs (copy/paste)
+        println!("Terminals:\n{sym_src}");
+    }
+    Ok((sym_src, lexgen.build_source_code(indent)))
+}
+
+pub fn write_lexilexer() {
+    let (result_sym, result_src) = lexilexer_source(LEXILEXER_LEXICON, 4, true)
+        .inspect_err(|e| eprintln!("Failed to parse lexicon: {e:?}"))
+        .unwrap();
+    replace_tagged_source(BUILD_LEXIPARSER_FILENAME, LEXI_SYM_TAG, &result_sym)
+        .expect("parser symbol replacement failed");
+    replace_tagged_source(LEXILEXER_FILENAME, LEXILEXER_TAG, &result_src)
+        .expect("lexer source replacement failed");
+}
+
+#[cfg(test)]
+mod tests {
+    use lexigram::test_tools::get_tagged_source;
+    use crate::*;
+    use super::*;
+
+    #[test]
+    fn test_source() {
+        let (_result_sym, _result_src) = lexilexer_source(LEXILEXER_LEXICON, 4, false)
+            .inspect_err(|e| eprintln!("Failed to parse lexicon: {e:?}"))
+            .unwrap();
+        if !cfg!(miri) {
+            let expected_sym = get_tagged_source(BUILD_LEXIPARSER_FILENAME, LEXI_SYM_TAG).unwrap_or(String::new());
+            let expected_src = get_tagged_source(LEXILEXER_FILENAME, LEXILEXER_TAG).unwrap_or(String::new());
+            assert_eq!(_result_sym, expected_sym);
+            assert_eq!(_result_src, expected_src);
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn write_source() {
+        write_lexilexer();
+    }
+}
---
+++ make-lexi/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod build_lexilexer;
+pub mod build_lexiparser;
+
+const LEXILEXER_LEXICON: &str = "./src/lexicon.l";
+const LEXILEXER_FILENAME: &str = "../lexi/src/lexilexer.rs";
+const LEXIPARSER_FILENAME: &str = "../lexi/src/lexiparser.rs";
+
+const LEXILEXER_TAG: &str = "lexilexer";
+const LEXIPARSER_TAG: &str = "lexiparser";
+
+const BUILD_LEXIPARSER_FILENAME: &str = "./src/build_lexiparser.rs";
+const LEXI_SYM_TAG: &str = "terminal_symbols";
