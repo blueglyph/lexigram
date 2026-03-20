023b0357ecb362e968027b518aa23b00311275da
Redglyph <redglyph@gmail.com>
2026-03-20 15:16:58 +0100

    generate pandemonium templates

--- examples/gen_pandemonium/src/lib.rs
+++ examples/gen_pandemonium/src/lib.rs
@@ -18,6 +18,7 @@ static LEXICON_FILENAME: &str = "src/pandemonium.l";
 static GRAMMAR_FILENAME: &str = "src/pandemonium.g";
 static NT_VALUES: [NTValue; 2] = [NTValue::Default, NTValue::None];
 static SOURCE_FILENAMES: [&str; 2] = ["../pandemonium/src/value.rs", "../pandemonium/src/no_value.rs"];
+static TMPL_FILENAMES: [&str; 2] = ["../pandemonium/src/tpl_value.txt", "../pandemonium/src/tpl_no_value.txt"];
 static LIBS: [&[&str]; 2] = [&["super::listener_types::*"], &[]];
 static LEXER_TAG: &str = "pandemonium_lexer";
 static PARSER_TAG: &str = "pandemonium_parser";
@@ -28,7 +29,7 @@ const PARSER_INDENT: usize = 4;
 
 fn gen_source_pandemonium(action: Action) {
     for i in 0..2 {
-        let (nt_value, source_filename, lib) = (&NT_VALUES[i], SOURCE_FILENAMES[i], LIBS[i].iter().map(|s| s.to_string()).to_vec());
+        let (nt_value, source_filename, tpl, lib) = (&NT_VALUES[i], SOURCE_FILENAMES[i], TMPL_FILENAMES[i], LIBS[i].iter().map(|s| s.to_string()).to_vec());
         if action == Action::Generate {
             println!("\n{:-<80}\nGenerating {source_filename}", "");
         }
@@ -40,6 +41,7 @@ fn gen_source_pandemonium(action: Action) {
             .libs(lib)
             .span_params(true)
             .set_nt_value(nt_value.clone())
+            .listener_code(gencode!(filename: tpl))
             .build()
             .expect("should have no error");
         match try_gen_parser(action, options) {
