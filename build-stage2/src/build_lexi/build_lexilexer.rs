eb75d56f7fc77e0cd2df6a2680e9544f8facdaf7
Redglyph <redglyph@gmail.com>
2026-04-09 12:07:36 +0200

    update version

--- build-stage2/src/build_lexi/build_lexilexer.rs
+++ build-stage2/src/build_lexi/build_lexilexer.rs
@@ -57,9 +57,9 @@ const EXPECTED_NBR_WARNINGS: usize = 0;
 fn lexilexer_source(indent: usize, _verbose: bool) -> Result<(BufLog, String), BufLog> {
     // [versions]
 
-    // lexigram-lib: 0.9.2
-    // lexi-gram: 0.9.2
-    // build-stage1: 0.9.2
+    // lexigram-lib: 0.9.3
+    // lexi-gram: 0.9.3
+    // build-stage1: 0.9.3
 
     // [versions]
 
