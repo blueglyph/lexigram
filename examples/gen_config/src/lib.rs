0aae6eb15493775b6a6eb656916696711b918987
Redglyph <redglyph@gmail.com>
2026-09-03 14:24:01 +0200

    merge lexicon and grammar of config example

--- examples/gen_config/src/lib.rs
+++ examples/gen_config/src/lib.rs
@@ -14,8 +14,7 @@ use lexi_gram::options::{Action, OptionsBuilder};
 use lexigram_lib::CollectJoin;
 use lexigram_lib::log::LogStatus;
 
-static LEXICON_FILENAME: &str = "src/config.l";
-static GRAMMAR_FILENAME: &str = "src/config.g";
+static LEXICON_GRAMMAR_FILENAME: &str = "src/config.lg";
 static SOURCE_FILENAME: &str = "../config/src/parser.rs";
 static LEXER_TAG: &str = "config_lexer";
 static PARSER_TAG: &str = "config_parser";
@@ -35,14 +34,16 @@ static NT_NAMES: [&str; 7] = [
         "-options",
 ];
 
+
 // -------------------------------------------------------------------------
 
-fn gen_source_config_l_g(action: Action) {
+fn gen_source_config_lg(action: Action) {
     let options = OptionsBuilder::new()
         .headers(["use lexi_gram::lexigram_lib::lexigram_core;"])
-        .lexer(genspec!(filename: LEXICON_FILENAME), gencode!(filename: SOURCE_FILENAME, tag: LEXER_TAG))
+        .combined_spec(genspec!(filename: LEXICON_GRAMMAR_FILENAME))
+        .lexer_code(gencode!(filename: SOURCE_FILENAME, tag: LEXER_TAG))
         .indent(LEXER_INDENT)
-        .parser(genspec!(filename: GRAMMAR_FILENAME), gencode!(filename: SOURCE_FILENAME, tag: PARSER_TAG))
+        .parser_code(gencode!(filename: SOURCE_FILENAME, tag: PARSER_TAG))
         .indent(PARSER_INDENT)
         .types_code(gencode!(filename: SOURCE_TEMPLATES, tag: USERS_TAG))
         .indent(INDENT_TEMPLATES)
@@ -58,61 +59,12 @@ fn gen_source_config_l_g(action: Action) {
             if action == Action::Generate {
                 println!("Code generated in {SOURCE_FILENAME}\n{log}");
             }
-            assert!(log.has_no_warnings(), "unexpected warning(s):\n{}", log.get_warnings().join("\n"));
-        }
-        Err(build_error) => panic!("{build_error}"),
-    }
-}
-
-mod tests_l_g {
-    use super::*;
-
-    #[test]
-    fn check_source() {
-        gen_source_config_l_g(Action::Verify);
-    }
-
-    #[ignore]
-    #[test]
-    fn write_source() {
-        gen_source_config_l_g(Action::Generate);
-    }
-}
-
-// -------------------------------------------------------------------------
-
-#[allow(unused)]
-static LEXICON_GRAMMAR_FILENAME: &str = "src/config.lg";
-
-// -------------------------------------------------------------------------
-
-#[cfg(any())]
-fn gen_source_config_lg(action: Action) {
-    let options = OptionsBuilder::new()
-        .combined_spec(genspec!(filename: LEXICON_GRAMMAR_FILENAME))
-        .lexer_code(gencode!(filename: SOURCE_FILENAME, tag: LEXER_TAG))
-        .indent(LEXER_INDENT)
-        .parser_code(gencode!(filename: SOURCE_FILENAME, tag: PARSER_TAG))
-        .indent(PARSER_INDENT)
-        .types_code(gencode!(filename: SOURCE_TEMPLATES, tag: USERS_TAG))
-        .indent(INDENT_TEMPLATES)
-        .listener_code(gencode!(filename: SOURCE_TEMPLATES, tag: LISTENER_TAG))
-        .indent(INDENT_TEMPLATES)
-        .libs(["super::listener_types::*"])
-        .build()
-        .expect("should have no error");
-    match try_gen_parser(action, options) {
-        Ok(log) => {
-            if action == Action::Generate {
-                println!("Code generated in {SOURCE_FILENAME}\n{log}");
-            }
             assert!(log.has_no_warnings(), "no warning expected");
         }
         Err(build_error) => panic!("{build_error}"),
     }
 }
 
-#[cfg(any())]
 mod test_lg {
     use super::*;
 
@@ -121,4 +73,9 @@ mod test_lg {
         gen_source_config_lg(Action::Verify);
     }
 
+    #[ignore]
+    #[test]
+    fn write_source() {
+        gen_source_config_lg(Action::Generate);
+    }
 }
